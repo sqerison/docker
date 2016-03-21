7104f81646492a1acb460b7585c2b2aab1e14b6b
sqerison <volodymyr.shynkar@gmail.com>
2016-03-21 13:00:25 +0200

    new script

--- prometheus/run.sh
+++ prometheus/run.sh
@@ -1,34 +1,12 @@
-docker stop cadvisor container-exporter prom-prometheus prom-alertmanager consul-server consul-registrator consul-exporter
-docker rm cadvisor container-exporter prom-prometheus prom-alertmanager consul-server consul-registrator consul-exporter
-set -x
-ip_addr=$(ifconfig eth0 | grep "inet addr" | awk '{print$2}'| cut -d ':' -f2)
-cp prometheus.yml.bac prometheus.yml
-sed -i s/localhost/$ip_addr/g prometheus.yml
-#
-docker run --name=cadvisor -d \
-              --restart=always \
-              -v /:/rootfs:ro \
-              -v /var/run:/var/run:rw \
-              -v /sys:/sys:ro \
-              -v /var/lib/docker/:/var/lib/docker:ro \
-              -p 8090:8080 \
-              google/cadvisor:latest
-#
-docker run --name container-exporter -d \
-              --restart=always \
-              -p 9104:9104 \
-              -v /sys/fs/cgroup:/cgroup \
-              -v /var/run/docker.sock:/var/run/docker.sock \
-              prom/container-exporter
-#
-docker run --name prom-alertmanager -d \
-              --restart=always \
-              -p 9093:9093 \
-              -v $PWD/alertmanager.conf:/tmp/alertmanager.conf \
-              prom/alertmanager \
-              -config.file=/tmp/alertmanager.conf
-#
-docker run --name consul-server -d \
+#!/usr/bin/env bash
+#set -x
+stack="consul-server cadvisor container-exporter prom-prometheus prom-alertmanager  consul-registrator consul-exporter"
+
+function start {
+       ip_addr=$(ifconfig eth0 | grep "inet addr" | awk '{print$2}'| cut -d ':' -f2)
+       cp prometheus.yml.bac prometheus.yml
+       sed -i s/localhost/$ip_addr/g prometheus.yml
+       docker run --name consul-server -d \
               --restart=always \
               -h node \
               -p 8500:8500 \
@@ -38,9 +16,32 @@ docker run --name consul-server -d \
               -server \
               -bootstrap \
               -advertise 127.0.0.1 \
-              -log-level debug
-#
-docker run --name consul-registrator -d \
+              -log-level debug &&
+
+       docker run --name=cadvisor -d \
+              --restart=always \
+              -v /:/rootfs:ro \
+              -v /var/run:/var/run:rw \
+              -v /sys:/sys:ro \
+              -v /var/lib/docker/:/var/lib/docker:ro \
+              -p 8090:8080 \
+              google/cadvisor:latest &&
+
+       docker run --name container-exporter -d \
+              --restart=always \
+              -p 9104:9104 \
+              -v /sys/fs/cgroup:/cgroup \
+              -v /var/run/docker.sock:/var/run/docker.sock \
+              prom/container-exporter &&
+
+       docker run --name prom-alertmanager -d \
+              --restart=always \
+              -p 9093:9093 \
+              -v $PWD/alertmanager.conf:/tmp/alertmanager.conf \
+              prom/alertmanager \
+              -config.file=/tmp/alertmanager.conf &&
+       
+       docker run --name consul-registrator -d \
               --restart=always \
               --name consul-registrator \
               --link consul-server:consul-server \
@@ -48,18 +49,18 @@ docker run --name consul-registrator -d \
               -h 127.0.0.1  \
               gliderlabs/registrator \
               consul://consul-server:8500 \
-              -deregister on-success
-#
-docker run --name consul-exporter -d \
+              -deregister on-success &&
+       
+       docker run --name consul-exporter -d \
               --restart=always \
               -p 9107:9107 \
               --link  consul-server:consul-server \
               --dns=$ip_addr \
               --dns-search=service.consul \
               prom/consul-exporter \
-              -consul.server=consul-server:8500
-#
-docker run --name prom-prometheus -d \
+              -consul.server=consul-server:8500 &&
+
+       docker run --name prom-prometheus -d \
               --restart=always \
               -p 9090:9090 \
               -v $PWD/prometheus.yml:/etc/prometheus/prometheus.yml \
@@ -70,4 +71,45 @@ docker run --name prom-prometheus -d \
               prom/prometheus \
               -config.file=/etc/prometheus/prometheus.yml \
               -alertmanager.url=http://localhost:9093
+}
+
+function stop {
+       docker stop $stack
+}
+
+function reload {
+       docker stop $stack 
+       docker start $stack
+}
+
+function restart {
+       docker stop $stack && \
+       docker rm $stack && \
+       start
+}
+
+function status {
+       echo -e "Container Name "'\t'"| Status"'\t'"| IP Address\n-\t-\t-\n$(docker inspect --format='{{.Name}}\t\t| {{.State.Status}}\t\t| {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}\n' $stack)" | \
+       column -ts $'\t\t'
+}
+
+case $1 in
+        start)
+                start
+                ;;
+        stop)
+                stop
+                ;;
+        reload)
+                reload
+                ;;
+        restart)
+                restart
+                ;;
+        status)
+                status
+                ;;
+        *)
+                echo "Start | Stop | Reload | Restart | Status"
+esac
 # END
